0f9a12d51877f4ad63ce275b505d1495b3aa1e92
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:17:11 -0700

    Created the MemoryAuthDataAccess

---
+++ server/src/main/java/dataAccess/MemoryAuthDataAccess.java
@@ -0,0 +1,30 @@
+package dataAccess;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.AuthData;
+
+public class MemoryAuthDataAccess implements AuthDataAccess {
+    private Map<String, AuthData> auths = new HashMap<String, AuthData>();
+
+    // Clear all auths.
+    public void clear() {
+        auths.clear();
+    }
+
+    // Create a new authorization.
+    public AuthData createAuth(AuthData auth) {
+        auths.put(auth.getAuthToken(), auth);
+        return auth;
+    }
+
+    // Retrieve an authorization given an authToken.
+    public AuthData getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    // Delete an authorization so that it is no longer valid.
+    public void deleteAuth(String authToken) {
+        auths.remove(authToken);
+    }
+}
