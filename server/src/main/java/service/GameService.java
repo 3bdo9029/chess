5813892997c6a6999f66af11c4ed6bd094ec4504
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:43:45 -0700

    Creating the auth data access file + have the completed GameService.java

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -22,4 +22,49 @@ public class GameService {
         }
         return new ListGamesResponse(gameDataAccess.listGames());
     }
+
+    public GameData createGame(String authToken, GameData gameData) throws ResponseException {
+        if (authDataAccess.getAuth(authToken) == null) {
+            throw new ResponseException(401, "unauthorized");
+        }
+        if (gameData.getGame() == null) {
+            gameData.setGame(new ChessGame());
+        }
+        return gameDataAccess.createGame(gameData);
+    }
+
+    public GameData joinGame(String authToken, JoinGameRequest joinGameRequest)
+            throws ResponseException {
+        AuthData auth = authDataAccess.getAuth(authToken);
+        if (auth == null) {
+            throw new ResponseException(401, "unauthorized");
+        }
+        GameData game = gameDataAccess.getGame(joinGameRequest.getGameId());
+        if (game == null) {
+            throw new ResponseException(400, "bad request");
+        }
+        if (joinGameRequest.getPlayerColor() == TeamColor.WHITE) {
+            if (auth.getUsername().equals(game.getWhiteUsername())) {
+                // Already joined, nothing to do. The request is idempotent.
+                return game;
+            } else if (game.getWhiteUsername() != null) {
+                throw new ResponseException(403, "already taken");
+            }
+            game.setWhiteUsername(auth.getUsername());
+        } else if (joinGameRequest.getPlayerColor() == TeamColor.BLACK) {
+            if (auth.getUsername().equals(game.getBlackUsername())) {
+                // Already joined, nothing to do. The request is idempotent.
+                return game;
+            } else if (game.getBlackUsername() != null) {
+                throw new ResponseException(403, "already taken");
+            }
+            game.setBlackUsername(auth.getUsername());
+        } else if (joinGameRequest.getPlayerColor() == null) {
+            // Add observer. Right now, there's nothing in the specification that
+            // actually requires this to be done. No "observer" fields required.
+        } else {
+            throw new ResponseException(400, "invalid color");
+        }
+        return gameDataAccess.updateGame(game.getGameId(), game);
+    }
 }
