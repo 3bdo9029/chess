6aea13b164c9acad7adc1aa1d91d9cf88596fda8
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:05:28 -0700

    Creating the paths

---
+++ server/src/main/java/ServerMain.java
@@ -0,0 +1,11 @@
+import server.Server;
+
+public class ServerMain {
+    private static Server server;
+
+    public static void main(String[] args) {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started server! Visit: http://localhost:" + port);
+    }
+}
