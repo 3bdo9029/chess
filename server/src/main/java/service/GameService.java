91d44eb2fd981513f100574bcce232b0306d1f56
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:39:34 -0700

    Creating the game Services filling p1 of the shell

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -8,5 +8,18 @@ import server.*;
 import chess.ChessGame;
 
 public class GameService {
+    private AuthDataAccess authDataAccess;
+    private GameDataAccess gameDataAccess;
 
+    public GameService(AuthDataAccess authDataAccess, GameDataAccess gameDataAccess) {
+        this.authDataAccess = authDataAccess;
+        this.gameDataAccess = gameDataAccess;
+    }
+
+    public ListGamesResponse listGames(String authToken) throws ResponseException {
+        if (authDataAccess.getAuth(authToken) == null) {
+            throw new ResponseException(401, "unauthorized");
+        }
+        return new ListGamesResponse(gameDataAccess.listGames());
+    }
 }
