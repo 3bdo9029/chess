60246db912aaad136c0e3ff89d2dfcbc41beac66
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:53:03 -0700

    Created the Data Access Exception file

---
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -0,0 +1,8 @@
+package dataAccess;
+
+import exception.ResponseException;
+
+interface DataAccess {
+    // A method for clearing all data from the database. Used during testing.
+    public void clear() throws ResponseException;
+}
