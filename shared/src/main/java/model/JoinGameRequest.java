ac23215ff28d0d2f292e74de8403922d48e07136
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-03 00:11:44 -0700

    Created the exception file, model, and shared server file

---
+++ shared/src/main/java/model/JoinGameRequest.java
@@ -0,0 +1,21 @@
+package model;
+
+import chess.ChessGame.TeamColor;
+
+public class JoinGameRequest {
+    private TeamColor playerColor;
+    private int gameID;
+
+    public JoinGameRequest(TeamColor playerColor, int gameID) {
+        this.playerColor = playerColor;
+        this.gameID = gameID;
+    }
+
+    public TeamColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public int getGameId() {
+        return gameID;
+    }
+}
---
+++ shared/src/main/java/model/ListGamesResponse.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Collection;
+
+public class ListGamesResponse {
+    private Collection<GameData> games;
+
+    public ListGamesResponse(Collection<GameData> games) {
+        this.games = games;
+    }
+
+    public Collection<GameData> getGames() {
+        return games;
+    }
+}
