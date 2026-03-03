a1a27699d162c36b9c7cd803c356e89333a6835f
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-02 23:28:00 -0700

    Creating Data Services java

---
+++ server/src/main/java/service/DataService.java
@@ -0,0 +1,24 @@
+package service;
+
+import dataAccess.*;
+import exception.ResponseException;
+
+public class DataService {
+    private UserDataAccess userDataAccess;
+    private AuthDataAccess authDataAccess;
+    private GameDataAccess gameDataAccess;
+
+    public DataService(
+            UserDataAccess userDataAccess, AuthDataAccess authDataAccess, GameDataAccess gameDataAccess) {
+        this.userDataAccess = userDataAccess;
+        this.authDataAccess = authDataAccess;
+        this.gameDataAccess = gameDataAccess;
+    }
+
+    public void clearData() throws ResponseException {
+        System.out.println("Clearing data...");
+        authDataAccess.clear();
+        gameDataAccess.clear();
+        userDataAccess.clear();
+    }
+}
