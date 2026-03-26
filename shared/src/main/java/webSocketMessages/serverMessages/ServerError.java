fce69786f6a90cfd9ae183a3b161c5a42a1296ac
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-25 22:13:25 -0600

    debug errors

---
+++ shared/src/main/java/webSocketMessages/serverMessages/ServerError.java
@@ -0,0 +1,12 @@
+package webSocketMessages.serverMessages;
+
+public class ServerError extends ServerMessage {
+    private String errorMessage;
+
+    public ServerError(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() { return errorMessage; }
+}
---
+++ shared/src/main/java/webSocketMessages/userCommands/UserGameCommand.java
@@ -0,0 +1,19 @@
+package webSocketMessages.userCommands;
+
+public class UserGameCommand {
+    public enum CommandType { CONNECT, MAKE_MOVE, LEAVE, RESIGN }
+
+    private CommandType commandType;
+    private String authToken;
+    private int gameID;
+
+    public UserGameCommand(CommandType commandType, String authToken, int gameID) {
+        this.commandType = commandType;
+        this.authToken = authToken;
+        this.gameID = gameID;
+    }
+
+    public CommandType getCommandType() { return commandType; }
+    public String getAuthToken() { return authToken; }
+    public int getGameID() { return gameID; }
+}
