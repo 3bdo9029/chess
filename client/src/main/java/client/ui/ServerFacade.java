2faa2835cab30c51ccf51b110fa4816de04b8f70
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-25 21:28:09 -0600

    Add ServerFacade class scaffold for client-server communication

---
+++ client/src/main/java/client/ui/ServerFacade.java
@@ -0,0 +1,13 @@
+package ui;
+
+import chess.ChessMove;
+import com.google.gson.Gson;
+import java.io.*;
+import java.net.*;
+import java.util.function.Consumer;
+import javax.websocket.*;
+import model.*;
+import webSocketMessages.userCommands.*;
+
+public class ServerFacade extends Endpoint {
+}
