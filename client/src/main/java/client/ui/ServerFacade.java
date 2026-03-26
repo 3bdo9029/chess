28ee92f6d17d06caf3accf00b27f29da17f2453c
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-25 21:33:08 -0600

    Add ServerFacade HTTP methods for user registration and login

--- client/src/main/java/client/ui/ServerFacade.java
+++ client/src/main/java/client/ui/ServerFacade.java
@@ -10,4 +10,33 @@ import model.*;
 import webSocketMessages.userCommands.*;
 
 public class ServerFacade extends Endpoint {
+    private static final Gson gson = new Gson();
+    private final String serverUrl;
+    private Session session;
+    private final Consumer<String> onMessage;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+        this.onMessage = null;
+    }
+
+    public ServerFacade(String serverUrl, Consumer<String> onMessage) {
+        this.serverUrl = serverUrl;
+        this.onMessage = onMessage;
+    }
+
+    public String getServerUrl() {
+        return this.serverUrl;
+    }
+
+    public AuthData registerUser(UserData user) {
+        var authData = fetch("POST", "/user", gson.toJson(user), null);
+        return gson.fromJson(authData, AuthData.class);
+    }
+
+    public AuthData loginUser(LoginRequest loginRequest) {
+        var authData = fetch("POST", "/session", gson.toJson(loginRequest), null);
+        return gson.fromJson(authData, AuthData.class);
+    }
+
 }
