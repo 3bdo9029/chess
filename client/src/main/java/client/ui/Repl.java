c0adbeb3fab5e8c77d68c078b5bf1c3990529239
Abe Farghali <abdurrahmanfarghali@gmail.com>
2026-03-25 20:28:23 -0600

    Add WebSocket message handling in REPL (LOAD_GAME, ERROR, NOTIFICATION)

--- client/src/main/java/client/ui/Repl.java
+++ client/src/main/java/client/ui/Repl.java
@@ -10,5 +10,42 @@ import model.*;
 import webSocketMessages.serverMessages.*;
 
 public class Repl {
+    private static final Gson gson = new Gson();
+    private ServerFacade serverFacade;
+    private AuthData authData;
+    private GameData gameData;
+    private Scanner scanner;
+
+    public Repl(String serverUrl) {
+        this.serverFacade = new ServerFacade(serverUrl, this::onMessage);
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void onMessage(String message) {
+        System.out.println("");
+        var serverMessage = gson.fromJson(message, ServerMessage.class);
+        switch (serverMessage.getServerMessageType()) {
+            case LOAD_GAME:
+            {
+                var load = gson.fromJson(message, LoadGame.class);
+                gameData = load.getGameData();
+                printGameData();
+                break;
+            }
+            case ERROR:
+            {
+                var error = gson.fromJson(message, ServerError.class);
+                System.err.println(error.getErrorMessage());
+                break;
+            }
+            case NOTIFICATION:
+            {
+                var notification = gson.fromJson(message, Notification.class);
+                System.out.println(notification.getMessage());
+                break;
+            }
+        }
+        System.out.print("> ");
+    }
 
 }
